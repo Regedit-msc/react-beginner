64514ab235069b4e307877781d276e75d39f7cb9
Regedit-msc <efusanyaae@gmail.com>
2020-11-05 21:11:33 +0100

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <Link className="brand-logo" to="/">
+          Poke' Times
+        </Link>
+        <ul className="right">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+// you can access props from the Browser Router to set up programmatic redirects ot use HOC withRouter
